bcf23c17f90a03e53dfade624a41d9739c5497cb
Harry Chen <czy88840616@gmail.com>
2020-05-31 23:22:08 +0800

    fix: fix egg config

--- packages/egg/src/config/config.default.ts
+++ packages/egg/src/config/config.default.ts
@@ -1,8 +1,20 @@
+const os = require('os');
+
+const tmpDir = os.tmpdir();
+
 export default {
   eggPlugins: {
     static: false,
     security: false,
     watcher: false,
     multipart: false,
+  },
+  eggPaths: [],
+  logger: {
+    dir: tmpDir,
+  },
+  rundir: tmpDir,
+  static: {
+    dir: tmpDir,
   }
 }
