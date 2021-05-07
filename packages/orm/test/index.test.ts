2aa0d4022fa88ac29b9b31045de7c1d6a0d68544
Harry Chen <czy88840616@gmail.com>
2021-05-07 22:47:28 +0800

    test: remove faas test

--- packages/orm/test/index.test.ts
+++ packages/orm/test/index.test.ts
@@ -1,38 +1,32 @@
-import { invoke } from '@midwayjs/serverless-invoke';
+import { IMidwayApplication, LightFramework } from '@midwayjs/core';
+import { createApp, close } from '@midwayjs/mock';
 import { join } from 'path';
 import * as assert from 'assert';
 import { existsSync, unlinkSync } from 'fs';
 
 describe('/test/index.test.ts', () => {
-  xit('should use one db', async () => {
+
+  it.skip('should use one db', async () => {
     cleanFile(join(__dirname, 'fixtures/base-fn', 'default.sqlite'));
-    process.env.MIDWAY_TS_MODE = 'true';
-    const result: any = await invoke({
-      functionDir: join(__dirname, 'fixtures/base-fn'),
-      functionName: 'test1',
-      data: [require('./http_meta.json')],
-      clean: false
-    });
-    process.env.MIDWAY_TS_MODE = 'false';
-    console.log('result.body', result);
-    assert(result && result.body.indexOf('hello world') > -1);
-    assert(result.body === 'hello world[[{"id":1,"name":"oneuser1"}],1]');
+    const app: IMidwayApplication = await createApp(join(__dirname, 'fixtures/base-fn'), {}, LightFramework);
+
+    const handlerFunction: any = await app.createAnonymousContext().requestContext.getAsync('indexHandler');
+    const result: any = await handlerFunction.run();
+    assert(result && result.indexOf('hello world') > -1);
+    assert(result === 'hello world[[{"id":1,"name":"oneuser1"}],1]');
   });
 
   it('use two db in one config', async () => {
     cleanFile(join(__dirname, 'fixtures/base-fn-multiple-db', 'default.sqlite'));
     cleanFile(join(__dirname, 'fixtures/base-fn-multiple-db', 'test.sqlite'));
-    process.env.MIDWAY_TS_MODE = 'true';
-    const result: any = await invoke({
-      functionDir: join(__dirname, 'fixtures/base-fn-multiple-db'),
-      functionName: 'test1',
-      data: [require('./http_meta.json')],
-      clean: false
-    });
-    process.env.MIDWAY_TS_MODE = 'false';
-    console.log('result.body', result);
-    assert(result && result.body.indexOf('hello world') > -1);
-    assert(result.body === 'hello world[[{"id":1,"name":"oneuser1"}],1]');
+
+    const app: IMidwayApplication = await createApp(join(__dirname, 'fixtures/base-fn-multiple-db'), {}, LightFramework);
+
+    const handlerFunction: any = await app.createAnonymousContext().requestContext.getAsync('indexHandler');
+    const result: any = await handlerFunction.run();
+    assert(result && result.indexOf('hello world') > -1);
+    assert(result === 'hello world[[{"id":1,"name":"oneuser1"}],1]');
+    await close(app);
   });
 });
 
