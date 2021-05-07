612606eed5112279b04b336836127d6f7c80f2ad
Harry Chen <czy88840616@gmail.com>
2021-05-07 23:50:26 +0800

    chore: ignore 10

--- packages/egg/test/index.test.ts
+++ packages/egg/test/index.test.ts
@@ -7,16 +7,17 @@ describe('/test/index.test.ts', () => {
   if (/^v10/.test(process.version)) {
     it('skip node v10', () => {});
     return;
-  }
-
-  it('should use origin http trigger in ice + faas demo by args', async () => {
-    process.env.MIDWAY_TS_MODE = 'true';
-    const result: any = await invoke({
-      functionDir: join(__dirname, 'fixtures/base-fn'),
-      functionName: 'test1',
-      data: [require('./http_meta.json')],
+  } else {
+    it('should use origin http trigger in ice + faas demo by args', async () => {
+      process.env.MIDWAY_TS_MODE = 'true';
+      const result: any = await invoke({
+        functionDir: join(__dirname, 'fixtures/base-fn'),
+        functionName: 'test1',
+        data: [require('./http_meta.json')],
+      });
+      process.env.MIDWAY_TS_MODE = 'false';
+      assert(result && result.body === 'hello world');
     });
-    process.env.MIDWAY_TS_MODE = 'false';
-    assert(result && result.body === 'hello world');
-  });
+  }
+  
 });
