40068b6e4c79a99c623e3c6676ebb80c90def168
Harry Chen <czy88840616@gmail.com>
2021-05-10 21:49:54 +0800

    chore: fix v10

--- packages/egg/test/index.test.ts
+++ packages/egg/test/index.test.ts
@@ -1,23 +1,21 @@
-import { invoke } from '@midwayjs/serverless-invoke';
 import { join } from 'path';
 import * as assert from 'assert';
 
 describe('/test/index.test.ts', () => {
 
-  if (/^v10/.test(process.version)) {
-    it('skip node v10', () => {});
-    return;
-  } else {
-    it('should use origin http trigger in ice + faas demo by args', async () => {
-      process.env.MIDWAY_TS_MODE = 'true';
-      const result: any = await invoke({
-        functionDir: join(__dirname, 'fixtures/base-fn'),
-        functionName: 'test1',
-        data: [require('./http_meta.json')],
-      });
-      process.env.MIDWAY_TS_MODE = 'false';
-      assert(result && result.body === 'hello world');
+  it('should use origin http trigger in ice + faas demo by args', async () => {
+    if (/^v10/.test(process.version)) {
+      return;
+    }
+    process.env.MIDWAY_TS_MODE = 'true';
+    const invoke = require('@midwayjs/serverless-invoke').invoke;
+    const result: any = await invoke({
+      functionDir: join(__dirname, 'fixtures/base-fn'),
+      functionName: 'test1',
+      data: [require('./http_meta.json')],
     });
-  }
-  
+    process.env.MIDWAY_TS_MODE = 'false';
+    assert(result && result.body === 'hello world');
+  });
+
 });
